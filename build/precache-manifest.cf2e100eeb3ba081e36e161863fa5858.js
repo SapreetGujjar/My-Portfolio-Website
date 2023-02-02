a6dcc91a847270abe83fb0ec6617cd1806390db3
Sabi GuJJar <99659601+SapreetGujjar@users.noreply.github.com>
2023-02-02 20:58:42 +0530

    Add files via upload

---
+++ build/precache-manifest.cf2e100eeb3ba081e36e161863fa5858.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff187e46fed0d2c66d3c",
+    "url": "/static/css/main.ad61e415.chunk.css"
+  },
+  {
+    "revision": "ff187e46fed0d2c66d3c",
+    "url": "/static/js/main.ff187e46.chunk.js"
+  },
+  {
+    "revision": "bfe1b30a9eaba948e3e4",
+    "url": "/static/js/1.bfe1b30a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7efb3ce1bfa3622fdaf573469377a613",
+    "url": "/index.html"
+  }
+];
